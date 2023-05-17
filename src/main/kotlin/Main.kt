15c6c3a56c69914d6ea580d37447f5481b7bb7d3
dimitriinc <dimitriinc@proton.me>
2023-05-17 09:22:46 -0500

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // FizzBuzz
+    for (i in 1..100) {
+        when {
+            i % 3 == 0 && i % 5 == 0 -> println("FizzBuzz")
+            i % 3 == 0 -> println("Fizz")
+            i % 5 == 0 -> println("Buzz")
+            else -> println(i)
+        }
+    }
+}
